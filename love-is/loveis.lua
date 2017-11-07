81fee7f4056ec88e2407b630eebe3cfe2be13ff1
Sean Conner <spc@conman.org>
2017-11-06 21:57:43 -0500

    Code to display dictionary.
    In case I need this again.

--- love-is/loveis.lua
+++ love-is/loveis.lua
@@ -32,89 +32,110 @@ local P  = lpeg.P
 local R  = lpeg.R
 local S  = lpeg.S
 
-local char      = (P"&" / "")
-                * (R("AZ","az")^1 / entity)
-                * (P";" / "")
-                + S[["*`]] / ""
-                + P(1)
-local cleanterm = Cs(char^1)
+local dict do
+  local char      = (P"&" / "")
+                  * (R("AZ","az")^1 / entity)
+                  * (P";" / "")
+                  + S[["*`]] / ""
+                  + P(1)
+  local cleanterm = Cs(char^1)
 
-local dchar     = (P"&" / "")
-                * (R("AZ","az")^1 / entity)
-                * (P";" / "")
-                + (P"<" * (P(1) - P">")^0 * P">") / ""
-                + P(1)
-local cleandef  = Cs(dchar^1)
+  local dchar     = (P"&" / "")
+                  * (R("AZ","az")^1 / entity)
+                  * (P";" / "")
+                  + (P"<" * (P(1) - P">")^0 * P">") / ""
+                  + P(1)
+  local cleandef  = Cs(dchar^1)
 
-local HW   = P"<hw>"
-local MHW  = P"<mhw>"
-local DEF  = P"<def>"
-local SHW  = P"</hw>"
-local SMHW = P"</mhw>"
-local SDEF = P"</def>"
+  local HW   = P"<hw>"
+  local MHW  = P"<mhw>"
+  local DEF  = P"<def>"
+  local SHW  = P"</hw>"
+  local SMHW = P"</mhw>"
+  local SDEF = P"</def>"
 
-local noHW     = (P(1) - (HW + MHW))^0
-local noSHW    = (P(1) - SHW)^0
-local noSDEF   = (P(1) - SDEF)^0
-local noHWDEF  = (P(1) - (HW + DEF))^0
-local noHWSMHW = (P(1) - (HW + SMHW))^0
+  local noHW     = (P(1) - (HW + MHW))^0
+  local noSHW    = (P(1) - SHW)^0
+  local noSDEF   = (P(1) - SDEF)^0
+  local noHWDEF  = (P(1) - (HW + DEF))^0
+  local noHWSMHW = (P(1) - (HW + SMHW))^0
 
-local hw  = HW            * C(noSHW)  * SHW
-local mhw = (HW * #R"!~") * C(noSHW)  * SHW
-local def = DEF           * C(noSDEF) * SDEF
+  local hw  = HW            * C(noSHW)  * SHW
+  local mhw = (HW * #R"!~") * C(noSHW)  * SHW
+  local def = DEF           * C(noSDEF) * SDEF
 
-local word   = Cg(hw * Ct((noHWDEF * def)^0))
-local mword  = MHW * noHW
-             * Cg(
-                     Ct((mhw * noHWSMHW)^1) * SMHW
-                   * Ct((noHWDEF * def)^0)
-                 )
-local doc    = Cf(
-                   Ct"" * (noHW * (mword + word))^1,
-                   function(acc,term,termdef)
-                     local function addword(w,defs)
-                       if not acc[w] then
-                         acc[w] = {}
-                       end
-                       
-                       for _,v in ipairs(defs) do
-                         table.insert(acc[w],v)
-                       end
-                     end
-                     
-                     local defs = {}
-                     for _,v in ipairs(termdef) do
-                       table.insert(defs,cleandef:match(v))
-                     end
-                     
-                     if type(term) == 'table' then
-                       for _,w in ipairs(term) do
-                         addword(cleanterm:match(w),defs)
-                       end
-                     else
-                       addword(cleanterm:match(term),defs)
-                     end
-                     return acc
-                   end
-               )
-               
-local raw do
-  raw = ""
-  for i = 1 , 25 do
-    local n = string.format("../refs/gcide_xml-0.51/xml_files/gcide_%s.xml",string.char(i + 97))
-    local f,e = io.open(n,"r")
-    if not f then
-      print(f,e)
-      os.exit(1)
+  local word   = Cg(hw * Ct((noHWDEF * def)^0))
+  local mword  = MHW * noHW
+               * Cg(
+                       Ct((mhw * noHWSMHW)^1) * SMHW
+                     * Ct((noHWDEF * def)^0)
+                   )
+                   
+  local doc = Cf(
+            Ct"" * (noHW * (mword + word))^1,
+            function(acc,term,termdef)
+              local function addword(w,defs)
+                if not acc[w] then
+                  acc[w] = {}
+                end
+                
+                for _,v in ipairs(defs) do
+                  table.insert(acc[w],v)
+                end
+              end
+              
+              local defs = {}
+              for _,v in ipairs(termdef) do
+                table.insert(defs,cleandef:match(v))
+              end
+              
+              if type(term) == 'table' then
+                for _,w in ipairs(term) do
+                  addword(cleanterm:match(w),defs)
+                end
+              else
+                addword(cleanterm:match(term),defs)
+              end
+              return acc
+            end
+         )
+         
+  local raw do
+    raw = ""
+    for i = 1 , 25 do
+      local n = string.format("../refs/gcide_xml-0.51/xml_files/gcide_%s.xml",string.char(i + 97))
+      local f,e = io.open(n,"r")
+      if not f then
+        print(f,e)
+        os.exit(1)
+      end
+      raw = raw .. f:read("*a")
+      f:close()
     end
-    raw = raw .. f:read("*a")
-    f:close()
   end
+
+  dict = doc:match(raw)
 end
 
-local dict = doc:match(raw)
-for _,def in pairs(dict) do
-  for _,d in ipairs(def) do
-    print(d)
+-- ************************************************************************
+
+do
+  local word = R("''","--","AZ","az")^1
+end
+
+
+do
+  local list = {}
+  for name in pairs(dict) do
+    table.insert(list,name)
+  end
+  
+  table.sort(list)
+  
+  for _,term in ipairs(list) do
+    print(term)
+    for _,definition in ipairs(dict[term]) do
+      print("",definition)
+    end
   end
 end
